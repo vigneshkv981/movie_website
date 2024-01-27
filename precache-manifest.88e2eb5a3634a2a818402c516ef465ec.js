60edffaff88be06ab936db33c5513618c4dc621f
vigneshtestengineer <vigneshkv6533@gmail.com>
2024-01-27 11:55:52 +0530

    Updates

---
+++ precache-manifest.88e2eb5a3634a2a818402c516ef465ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72cc1c65e5415c289789bbc159a140c",
+    "url": "/movie_website/index.html"
+  },
+  {
+    "revision": "61f1d5da9bd77a36f15a",
+    "url": "/movie_website/static/css/main.2fd6302c.chunk.css"
+  },
+  {
+    "revision": "2e19c49a5c4791a68236",
+    "url": "/movie_website/static/js/2.92564688.chunk.js"
+  },
+  {
+    "revision": "65a9926ecde5afbe3bda",
+    "url": "/movie_website/static/js/3.3b6fb9ae.chunk.js"
+  },
+  {
+    "revision": "61f1d5da9bd77a36f15a",
+    "url": "/movie_website/static/js/main.ce84b07a.chunk.js"
+  },
+  {
+    "revision": "8fb33153bd2dccff2e67",
+    "url": "/movie_website/static/js/runtime~main.d4933701.js"
+  }
+]);
